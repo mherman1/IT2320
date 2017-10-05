07a65dab017d4243c0c19ae5f9d7b3776803c355
M <misko1099@hotmail.com>
2017-10-04 23:36:22 -0400

    A05
    Assignment 5

---
+++ Assignments/A05/CheckerBoard/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+    $(".red").click(function () {
+
+        $(this).css("background-color", "yellow");
+
+        $(".black,.cell").click(function ()
+        {
+            $(this).removeClass("piece black").addClass("piece red");
+        });
+
+        $(this).click(function ()
+        {
+            for (var i = 0; i < cells.length; i++)
+            {
+                var cell = $(cells[i]);
+                var isDark = colorCount % 2 == 0;
+                var isNextRow = (i + 1) % 8 == 0;
+                colorCount += isNextRow ? 2 : 1;
+                cell.css("background-color", isDark ? "navy" : "white");
+            }
+        });
+    });
+
+    $(".black").click(function () {
+
+        $(this).css("background-color", "yellow");
+
+        $(".red,.cell").click(function () {
+            $(this).removeClass("piece red").addClass("piece black");
+        });
+
+        $(this).click(function () {
+            for (var i = 0; i < cells.length; i++) {
+                var cell = $(cells[i]);
+                var isDark = colorCount % 2 == 0;
+                var isNextRow = (i + 1) % 8 == 0;
+                colorCount += isNextRow ? 2 : 1;
+                cell.css("background-color", isDark ? "navy" : "white");
+            }
+        });
+    });
+
+});
