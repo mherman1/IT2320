3bbdef08012df0a5e74bff0598f651b64e5baf1b
M <misko1099@hotmail.com>
2017-11-23 22:33:13 -0500

    Assignment 13
    Assignment 13

--- Assignments/FinalProject/FinalProject/Scripts/Home.js
+++ Assignments/FinalProject/FinalProject/Scripts/Home.js
@@ -1,1 +1,6 @@
-﻿
+﻿$(document).ready(function () {
+    $(".button").click(function () {
+        $(".home").animate({ right: '1040px' });
+        $(".account").animate({ right: '1560px' });
+    });
+});
